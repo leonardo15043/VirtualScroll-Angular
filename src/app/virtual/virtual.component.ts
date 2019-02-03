709866674813388f20dc0bf912d46291f0638b42
leonardo15043 <lahernandez15043@gmail.com>
2019-02-02 20:50:00 -0500

    Controles para moverse en la lista

--- src/app/virtual/virtual.component.ts
+++ src/app/virtual/virtual.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { CdkVirtualScrollViewport } from '@angular/cdk/scrolling';
 
 @Component({
   selector: 'app-virtual',
@@ -7,12 +8,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class VirtualComponent implements OnInit {
 
+  @ViewChild( CdkVirtualScrollViewport ) viewport: CdkVirtualScrollViewport;
+
   personas = Array(500).fill(0);
 
   constructor() { }
 
   ngOnInit() {
     console.log(this.personas);
+  }
+
+  irFinal() {
+    this.viewport.scrollToIndex( this.personas.length );
+  }
+
+  irInicio() {
+    this.viewport.scrollToIndex( 0 );
+  }
+
+  irCentro() {
+    this.viewport.scrollToIndex( this.personas.length / 2 );
   }
 
 }
